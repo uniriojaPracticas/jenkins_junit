cb85f49ed445a6bb993a42b680c242c63613131a
Alumno <Alumno@MASTERDMC-PC03>
2017-05-02 19:57:24 +0200

    First

---
+++ jenkins-junit/src/main/java/User.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+
+public class User {
+	private String name;
+	private Integer age;
+	private Date birth;
+	
+	public User(String name, Integer age, Date birth) {
+		this.name = name;
+		this.age = age;
+		this.birth = birth;
+	}
+	public User() {
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	
+}
---
+++ jenkins-junit/src/test/java/UserTester.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
+public class UserTester {
+	
+	private User user;
+	
+	public UserTester() {
+		user = new User("Julio", 25, new GregorianCalendar(1993, Calendar.SEPTEMBER, 7).getTime());
+	}
+
+	@Test
+	public void testGetName() {
+		assertSame("Julio", user.getName());
+	}
+
+	@Test
+	public void testSetName() {
+		user.setName("Ana");
+		assertSame("Ana", user.getName());
+	}
+
+	@Test
+	public void testGetAge() {
+		assertSame(25, user.getAge());
+	}
+
+	@Test
+	public void testSetAge() {
+		user.setAge(20);
+		assertSame(20, user.getAge());
+	}
+
+	@Test
+	public void testGetBirth() {
+		Date birth = new GregorianCalendar(1993, Calendar.SEPTEMBER, 7).getTime();
+		assertEquals(birth, user.getBirth());
+	}
+
+	@Test
+	public void testSetBirth() {
+		Date birth = new GregorianCalendar(1993, Calendar.APRIL, 22).getTime();
+		user.setBirth(birth);
+		assertEquals(birth, user.getBirth());
+	}
+
+}
